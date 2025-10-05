6e2d519d6cfdb95563ff86c90a9cea290a2f9b6e
Varun Verma <varun.verma2024@gmail.com>
2025-10-05 17:07:35 +0530

    created basic rest routes for users controller

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,40 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
+
+class createUserDTO {
+  name: string;
+  email: string;
+  role: string;
+}
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+
+  // GET routes
+  @Get()
+  findUsers() {
+    return [];
+  }
+
+  @Get(':id')
+  findUserById(@Param('id') userId: string) {
+    return userId;
+  }
+
+  // POST routes
+  @Post()
+  makeUser(@Body() userData: createUserDTO) {
+    return userData;
+  }
+
+  // Patch routes
+  @Patch(':id')
+  updateUser(@Param('id') userId: string, @Body() updatedData: createUserDTO) {
+    return { userId, ...updatedData };
+  }
+
+  // Delete routes
+  @Delete(':id')
+  deleteUser(@Param('id') userId: string) {
+    return { deleted: userId };
+  }
+}
